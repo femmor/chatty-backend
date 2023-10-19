4c00279ef150b1612727997b66c47e3da4b39d04
Omolara1950 <fegomson@gmail.com>
2023-10-19 11:20:20 -0500

    Standard and security middleware setup

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -7,6 +7,14 @@ import {
   NextFunction,
 } from "express";
 
+import cookieSession = require("cookie-session");
+import cors = require("cors");
+import hpp = require("hpp");
+import compression = require("compression");
+import helmet from "helmet";
+import "express-async-errors";
+import HTTP_STATUS from "http-status-codes";
+
 import { Server } from "http";
 
 export class ChattyServer {
@@ -20,17 +28,50 @@ export class ChattyServer {
     this.securityMiddleware(this.app);
     this.standardMiddleware(this.app);
     this.routesMiddleware(this.app);
-    this.globalHandler(this.app);
+    this.globalErrorHandler(this.app);
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: ["test1", "test2"],
+        maxAge: 24 * 7 * 3600,
+        secure: false,
+      })
+    );
 
-  private standardMiddleware(app: Application): void {}
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      })
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(
+      json({
+        limit: "50mb",
+      })
+    );
+    app.use(
+      urlencoded({
+        extended: true,
+        limit: "50mb",
+      })
+    );
+  }
 
   private routesMiddleware(app: Application): void {}
 
-  private globalHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {}
 
   private startServer(app: Application): void {}
 
