33da5abd8b3c269e30a11cbdd9fd57663b65bf53
Omolara1950 <fegomson@gmail.com>
2023-10-23 09:28:49 -0500

    Joi validator method

---
+++ src/shared/globals/decorators/joi-validation.decorators.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { JoiRequestValidationError } from '@globals/helpers/error-handler';
+import { Request } from 'express';
+import { ObjectSchema } from 'joi';
+
+type IJoiDecorator = (target: any, key: string, descriptor: PropertyDescriptor) => void;
+
+export function joiValidator(schema: ObjectSchema): IJoiDecorator {
+	return (_target: any, _key: string, descriptor: PropertyDescriptor) => {
+		const originalMethod = descriptor.value;
+		descriptor.value = async function (...args: any[]) {
+			const request: Request = args[0];
+			const body = request.body;
+			const { error } = await Promise.resolve(schema.validate(body));
+			if (error?.details) {
+				throw new JoiRequestValidationError(error.details[0].message);
+			}
+			return await originalMethod.apply(this, args);
+		};
+		return descriptor;
+	};
+}
