79d40b8c552b8d94fdd9efccd91ac68214cf0581
Omolara1950 <fegomson@gmail.com>
2023-10-23 10:15:49 -0500

    Auth interface and model schema

---
+++ src/features/auth/interfaces/auth.interface.ts
@@ -0,0 +1,51 @@
+import { Document } from 'mongoose';
+import { ObjectId } from 'mongodb';
+
+// Adds the currentUser property to the Express Request object
+declare global {
+	namespace Express {
+		interface Request {
+			currentUser?: AuthPayload;
+		}
+	}
+}
+
+// Defines the interface for the AuthPayload object
+export interface AuthPayload {
+	userId: string;
+	uId: string;
+	email: string;
+	username: string;
+	avatarColor: string;
+	iat?: number;
+}
+
+// Defines the interface for the AuthDocument object
+export interface IAuthDocument extends Document {
+	_id: string | ObjectId;
+	uId: string;
+	username: string;
+	email: string;
+	password?: string;
+	avatarColor: string;
+	createdAt: Date;
+	passwordResetToken?: string;
+	passwordResetExpires?: number | string;
+	comparePassword(password: string): Promise<boolean>;
+	hashPassword(password: string): Promise<string>;
+}
+
+// Defines the interface for the AuthModel object
+export interface ISignUpData {
+	_id: ObjectId;
+	uId: string;
+	email: string;
+	username: string;
+	password: string;
+	avatarColor: string;
+}
+
+// Defines the interface for the Auth job object
+export interface IAuthJob {
+	value?: string | IAuthDocument;
+}
---
+++ src/features/auth/models/auth.schema.ts
@@ -0,0 +1,50 @@
+import { IAuthDocument } from '@auth/interfaces/auth.interface';
+import { hash, compare } from 'bcryptjs';
+import { model, Model, Schema } from 'mongoose';
+
+// Bcryptjs salt round
+const SALT_ROUND = 10;
+
+const authSchema: Schema = new Schema(
+	{
+		username: { type: String },
+		uId: { type: String },
+		email: { type: String },
+		password: { type: String },
+		avatarColor: { type: String },
+		createdAt: { type: Date, default: Date.now },
+		passwordResetToken: { type: String, default: '' },
+		passwordResetExpires: { type: Number }
+	},
+	// Deletes the password from the response and returns the document
+	{
+		toJSON: {
+			transform(_doc, ret) {
+				delete ret.password;
+				return ret;
+			}
+		}
+	}
+);
+
+// Hash the password before saving it to the database when user is registering
+authSchema.pre('save', async function (this: IAuthDocument, next: () => void) {
+	const hashedPassword: string = await hash(this.password as string, SALT_ROUND);
+	this.password = hashedPassword;
+	next();
+});
+
+// Compare the password with the hashed password
+authSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+	const hashedPassword: string = (this as unknown as IAuthDocument).password!;
+	return compare(password, hashedPassword);
+};
+
+// Hash the password before saving it to the database when user is updating their profile
+authSchema.methods.hashPassword = async function (password: string): Promise<string> {
+	return hash(password, SALT_ROUND);
+};
+
+// Exports the AuthModel object
+const AuthModel: Model<IAuthDocument> = model<IAuthDocument>('Auth', authSchema, 'Auth');
+export { AuthModel };
